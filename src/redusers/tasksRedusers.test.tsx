b0082c3ebb55985aba2dc9d093b412ce45113537
EvgeniBlag <blagodarni@gmail.com>
2022-08-09 09:46:58 +0200

    TestRemoveTask

--- src/redusers/tasksRedusers.test.tsx
+++ src/redusers/tasksRedusers.test.tsx
@@ -13,24 +13,22 @@ let id:string
 
 beforeEach(()=>{
 
-    let todolistId1=v1()
-    let todolistId2=v1()
-    let id=v1()
+   
 
     state =  {
-        [todolistID1]: [
-            { id: v1(), title: "HTML&CSS", isDone: true },
-            { id: v1(), title: "JS", isDone: true },
-            { id: v1(), title: "ReactJS", isDone: false },
-            { id: v1(), title: "Rest API", isDone: false },
-            { id: v1(), title: "GraphQL", isDone: false },
+        'todolistID1': [
+            { id: '1', title: "HTML&CSS", isDone: true },
+            { id: '2', title: "JS", isDone: true },
+            { id: '3', title: "ReactJS", isDone: false },
+            { id: '4', title: "Rest API", isDone: false },
+            { id: '5', title: "GraphQL", isDone: false },
         ],
-        [todolistID2]: [
-            { id: v1(), title: "HTML&CSS2", isDone: true },
-            { id: v1(), title: "JS2", isDone: true },
-            { id: v1(), title: "ReactJS2", isDone: false },
-            { id: v1(), title: "Rest API2", isDone: false },
-            { id: v1(), title: "GraphQL2", isDone: false },
+        'todolistID2': [
+            { id: '6', title: "HTML&CSS2", isDone: true },
+            { id: '7', title: "JS2", isDone: true },
+            { id: '8', title: "ReactJS2", isDone: false },
+            { id: '9', title: "Rest API2", isDone: false },
+            { id: '10', title: "GraphQL2", isDone: false },
         ]
     }
 })
@@ -41,10 +39,27 @@ test("Test para eliminar corectamente Task",()=>{
     
     //TestFunction2:
 
-   const endState = tasksReducer(state,removeTaskAC(todolistID1,id))
+   const endState = tasksReducer(state,removeTaskAC('todolistID1','2'))
 
    //Test3:
-
-  
-
-})
+ 
+  expect(endState).toEqual(
+    {
+        'todolistID1': [
+            { id:'1', title: "HTML&CSS", isDone: true },
+            // { id: v1(), title: "JS", isDone: true },
+            { id: '3', title: "ReactJS", isDone: false },
+            { id: '4', title: "Rest API", isDone: false },
+            { id: "5", title: "GraphQL", isDone: false },
+        ],
+        'todolistID2': [
+            { id: '6', title: "HTML&CSS2", isDone: true },
+            { id: '7', title: "JS2", isDone: true },
+            { id: '8', title: "ReactJS2", isDone: false },
+            { id: '9', title: "Rest API2", isDone: false },
+            { id: '10', title: "GraphQL2", isDone: false },
+        ]
+    }
+  )
+}
+)
